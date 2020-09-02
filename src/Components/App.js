4afcaa90d367571173824c86fa46ccedd761e3e1
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-02 17:00:06 +0530

    Added all imports to the App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-
+import PlayerList from './Player/PlayerList';
+import PlayerSingle from './Player/PlayerSingle';
+import PlayerList from './Player/PlayerForm';
 
 function App() {
   return (
-    <div className="App">
-      Helloo
-    </div>
+   <div className="container-fluid">
+     <div className="row">
+        <div className="col s12">Menu</div>
+     </div>
+   
+     <div className="row">
+        <div className="col s3"><PlayerList/></div>
+        <div className="col s9"><PlayerSingle/></div>
+     </div>
+
+   </div>
   );
 }
 
