fe735ce9db2c2f5a3f94480b13052fe728c61fe2
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-02 16:42:43 +0530

    Created basic PlayerList and PlayerSingle component

---
+++ src/Components/Player/PlayerList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PlayerList=()=>{
+    return(
+        <div>
+            PlayerList
+        </div>
+    )
+}
+
+
+export default PlayerList;
---
+++ src/Components/Player/PlayerSingle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const PlayerSingle=()=>{
+    return(
+        <div>
+            PlayerSingle
+        </div>
+    )
+}
+
+export default PlayerSingle;
