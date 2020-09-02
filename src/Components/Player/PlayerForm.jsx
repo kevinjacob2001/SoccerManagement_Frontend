adcb24b4dca8a8231f5fe10e9f001ebef1945cb9
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-02 16:45:13 +0530

    Created the basic PlayerForm component

--- src/Components/Player/PlayerForm.jsx
+++ src/Components/Player/PlayerForm.jsx
@@ -1,2 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
 
+class PlayerForm extends Component{
+    render(){
+        return(
+            <div>
+                Player Form
+            </div>
+        )
+    }
+}
 
+export default PlayerForm;
