1beb650541429a3c6b2162515201d00ebce979e2
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-09-02 21:31:23 +0530

    Added basic form

--- src/Components/Player/PlayerForm.jsx
+++ src/Components/Player/PlayerForm.jsx
@@ -2,11 +2,43 @@ import React from 'react';
 import { Component } from 'react';
 
 class PlayerForm extends Component{
+    constructor(){
+        super()
+    }
     render(){
         return(
-            <div>
-                Player Form
-            </div>
+           
+  <div className="row">
+    <form className="col s12">
+        <div className="row">
+        <div className="input-field col s6">
+            <input placeholder="Placeholder" id="first_name" type="text" class="validate"/>
+            <label for="first_name">First Name</label>
+        </div>
+        <div className="input-field col s6">
+            <input id="last_name" type="text" class="validate"/>
+            <label for="last_name">Last Name</label>
+        </div>
+        <div className="input-field col s6">
+            <input id="last_name" type="text" class="validate"/>
+            <label for="last_name">Last Name</label>
+        </div>
+        <div className="input-field col s6">
+            <input id="last_name" type="text" class="validate"/>
+            <label for="last_name">Last Name</label>
+        </div>
+        <div className="input-field col s6">
+            <input id="last_name" type="text" class="validate"/>
+            <label for="last_name">Last Name</label>
+        </div>
+        <div className="input-field col s6">
+            <input id="last_name" type="text" class="validate"/>
+            <label for="last_name">Last Name</label>
+        </div>
+        
+        </div>
+    </form>
+</div>
         )
     }
 }
